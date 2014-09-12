c3fb625db5dcb32c104f59e7b90db283574a869b
Henrique <henrique@henriqueog.com>
2014-09-12 10:48:28 -0300

    Update FresnelShader.java
    add color option (for the color of the edges and the surface color) by SC Parser

--- src/org/sunflow/core/shader/FresnelShader.java
+++ src/org/sunflow/core/shader/FresnelShader.java
@@ -1,61 +1,65 @@
 package org.sunflow.core.shader;
- 
+
 import org.sunflow.core.ShadingState;
 import org.sunflow.image.Color;
 import org.sunflow.core.ParameterList;
 import org.sunflow.SunflowAPI;
 import org.sunflow.core.shader.*;
+import org.sunflow.core.shader.DiffuseShader;
+import org.sunflow.core.Shader;
+
 public class FresnelShader implements Shader {
- 
-public Color edgeColor;
-public Color surfaceColor;
-private DiffuseShader light;
-private DiffuseShader dark;
-private Int samples;
- 
-public FresnelShader()
-{
-  edgeColor = Color.WHITE;
-  surfaceColor = Color.GRAY;
-  light = new DiffuseShader();
-  dark = new DiffuseShader();
-}
- 
-  public boolean update(ParameterList pl, SunflowAPI api) {
-        edgeColor = pl.getColor("edge_color", edgeColor);
-        surfaceColor = pl.getColor("surface_color", surfaceColor);
-        return true;
+
+  public Color edgeColor;
+  public Color surfaceColor;
+  private DiffuseShader light;
+  private DiffuseShader dark;
+  private int samples;
+
+  public FresnelShader()
+  {
+    light = new DiffuseShader();
+    dark = new DiffuseShader();
   }
- 
- public Color getRadiance(ShadingState state) {
+
+  public boolean update(ParameterList pl, SunflowAPI api) {
+    edgeColor = pl.getColor("edge_color", edgeColor);
+    surfaceColor = pl.getColor("surface_color", surfaceColor);
+
+    pl.addColor( "diffuse", surfaceColor);
+    boolean b2 = dark.update(pl, api);
+
+    pl.addColor( "diffuse", edgeColor);
+    boolean b1 = light.update(pl, api);
+
+    return (b1 && b2);
+
+        //return true;
+  }
+
+  public Color getRadiance(ShadingState state) {
   //olhar para a face correta
-  state.faceforward();
-  state.initLightSamples();
-   
-  float c=state.getCosND();
- 
+    state.faceforward();
+    float c=state.getCosND();
+
   //original
   //Color l = new Color( light.getRadiance(state) );
   //Color d = new Color( dark.getRadiance(state) );
-   
-  Color l = new Color(edgeColor);
-  Color d = new Color(surfaceColor);
- 
-  return Color.blend(l, d, c);
- }
- 
- public void scatterPhoton(ShadingState state, Color power) {
-     light.scatterPhoton(getDiffuse(state), power); //do we need to do this?
-     dark.scatterPhoton(getDiffuse(state), power);
-    }
- 
-    public Color getDiffuse(ShadingState state) {
-     pl.addColor( "diffuse", surfaceColor);
-     boolean b2 = dark.update(pl, api);
-      
-     pl.addColor( "diffuse", edgeColor);
-     boolean b1 = light.update(pl, api);
-      
-     return (b1 && b2);
-    }
+
+    Color l = new Color();
+    Color d = new Color();
+    l.set(light.getRadiance(state));
+    d.set(dark.getRadiance(state));
+
+    return Color.blend(l, d, c);
+  }
+
+  public void scatterPhoton(ShadingState state, Color power) {
+     light.scatterPhoton(state, power); //do we need to do this?
+     dark.scatterPhoton(state, power);
    }
+
+    //public Color getDiffuse(ShadingState state) {
+//
+    //}
+ }
